1459ccd73381544fc8cd9aea643659cde28f6d8b
mrene42 <mariarenebarco.969@gmail.com>
2025-02-07 17:53:56 +0100

    feat: add model "toll"

--- app/Models/Toll.php
+++ app/Models/Toll.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Toll extends Model
 {
-    //
+    protected $fillable = [
+        "name",
+        "city",
+        "collection"
+    ];
+
+    public function vehicles ()
+    {
+        return $this->belongsToMany(Vehicle::class, 'vehicles_tolls');
+    }
 }
