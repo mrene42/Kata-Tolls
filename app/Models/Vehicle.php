3e141729c039641240a5cf7a5e71353040d1f3ba
mrene42 <mariarenebarco.969@gmail.com>
2025-02-07 17:57:31 +0100

    feat: add model "vehicle"

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vehicle extends Model
 {
-    //
+    protected $fillable = [
+        "type_id",
+        "license",
+        "total_expense"
+    ];
+
+    public function type ()
+    {
+        return $this->belongsTo(Type::class, "type_id");
+    }
+
+    public function tolls ()
+    {
+        return $this->belongsToMany(Toll::class, 'vehicles_tolls');
+    }
 }
